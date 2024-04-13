5710b4c4996629f5bf561a7f10bb0eef6550b370
Kacper <kacper.laudanski@wp.pl>
2024-04-13 22:25:23 +0200

    register page structure

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,11 +1,15 @@
 import { cva } from "class-variance-authority";
 import type { VariantProps } from "class-variance-authority";
 import { ComponentProps } from "react";
+import ShowPasswordIcon from "./icons/ShowPasswordIcon";
 
 const inputStyles = cva([], {
   variants: {
     page: {
       loginForm: ["px-4 w-full border-2 py-2 rounded-md text-sm outline-none"],
+      registerForm: [
+        "mt-2 p-2 border border-gray-300 focus:outline-none focus:ring-0 focus:border-gray-300 rounded text-sm text-gray-900",
+      ],
     },
   },
 });
@@ -14,11 +18,17 @@ type Input = VariantProps<typeof inputStyles> & ComponentProps<"input">;
 
 export default function Input({ page, ...props }: Input) {
   return (
-    <div className="mt-5">
-      <label className="block text-md mb-2" htmlFor={props.name}>
+    <div className="flex flex-col mt-5 relative">
+      <label className="text-md mb-2" htmlFor={props.id}>
         {props.name}
       </label>
       <input {...props} className={inputStyles({ page })} />
+      {props.type === "password" && (
+        <button
+          type="button"
+          className="absolute bottom-2.5 right-2 bg-transparent flex items-center justify-center text-gray-700"
+        ><ShowPasswordIcon /></button>
+      )}
     </div>
   );
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,9 +3,9 @@ import LoginForm from "../components/login/LoginForm";
 
 export default function LoginPage() {
   return (
-    <main className={`min-h-screen`}>
+    <main className={`bg-neutral-100 min-h-screen`}>
       <div className="flex justify-center w-full">
-        <div className="bg-white min-h-screen w-3/4 flex justify-center items-center">
+        <div className="min-h-screen w-3/4 flex justify-center items-center">
           <div className="sm:w-full md:w-1/2">
             <LoginForm /> 
             <p className="mt-8">
