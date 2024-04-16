6567dc94917fc28332808909fbd02e27d3fb8136
Kacper <kacper.laudanski@wp.pl>
2024-04-16 21:41:02 +0200

    sidebar menu animation and styling

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -2,11 +2,11 @@ import { cva, VariantProps } from "class-variance-authority";
 import { ComponentProps } from "react";
 
 
-const buttonStyle = cva([], {
+const buttonStyle = cva(['transition-all duration-300 flex items-center'], {
     variants:{
        variant:{
-        navbarExtended:["w-full h-[40px] bg-neutral-100 rounded-md text-sm"],
-        navbarRolled:[]
+        navbarExtended:["w-full h-[40px] bg-neutral-100 rounded-md text-sm px-4 justify-between"],
+        navbarRolled:['w-full h-[65px] bg-neutral-100 rounded-md justify-center']
        }
     }
 })
