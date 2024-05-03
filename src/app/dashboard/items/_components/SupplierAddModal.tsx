6896151a5425292488e27e84fae8c32627dc8403
Kacper <kacper.laudanski@wp.pl>
2024-05-03 22:50:14 +0200

    suppliers form done

---
+++ src/app/dashboard/items/_components/SupplierAddModal.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { PackageSearch } from "lucide-react";
+import { useEffect, useRef } from "react";
+import Button from "../../../components/Button";
+import FormHeader from "../../../components/form/FormHeader";
+import Input from "../../../components/form/Input";
+import SupplierAddForm from "./SupplierAddForm";
+
+interface SuppliersAddModal {
+  openModal: boolean;
+  onCloseModal: () => void;
+}
+
+export default function SupplierAddModal({
+  openModal,
+  onCloseModal,
+}: SuppliersAddModal) {
+  const ref = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (openModal) return ref.current?.showModal();
+    if (!openModal) return ref.current?.close();
+  }, [openModal]);
+
+  return (
+    <dialog
+      ref={ref}
+      onClose={onCloseModal}
+      className={"w-1/2 max-w-[800px] h-1/2 max-h-[500px] bg-neutral-100 rounded-lg"}
+    >
+      <FormHeader name={'ADD SUPPLIER'} onCloseModal={onCloseModal} />
+      <SupplierAddForm /> 
+    </dialog>
+  );
+}
