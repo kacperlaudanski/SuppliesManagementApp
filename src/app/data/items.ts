49914c5cd68119de3d20586b45cb5926fb743075
Kacper <kacper.laudanski@wp.pl>
2024-08-13 10:58:42 +0200

    Added items and suppliers hardcoded data

---
+++ src/app/data/items.ts
@@ -0,0 +1,46 @@
+export const items = [
+    {
+        name: 'Ziemia A', 
+        quantity: 15, 
+        minQuantity: 3, 
+        optimalPeriod: 10, 
+        maxQuantity: 30, 
+        image: '../../somePath', 
+        supplier: {}, 
+        category: {}, 
+        description: 'bla bla bla'
+    },
+    {
+        name: 'Ziemia B', 
+        quantity: 6, 
+        minQuantity: 2, 
+        optimalPeriod: 8, 
+        maxQuantity: 19, 
+        image: '../../somePath', 
+        supplier: {}, 
+        category: {}, 
+        description: 'bla bla bla'
+    },
+    {
+        name: 'Cegła A', 
+        quantity: 20, 
+        minQuantity: 6, 
+        optimalPeriod: 5, 
+        maxQuantity: 50, 
+        image: '../../somePath', 
+        supplier: {}, 
+        category: {}, 
+        description: 'bla bla bla'
+    },
+    {
+        name: 'Cement', 
+        quantity: 9, 
+        minQuantity: 2, 
+        optimalPeriod: 8, 
+        maxQuantity: 27, 
+        image: '../../somePath', 
+        supplier: {}, 
+        category: {}, 
+        description: 'bla bla bla'
+    }
+]
---
+++ src/app/data/suppliers.ts
@@ -0,0 +1,5 @@
+export const suppliers = [
+    {
+        name: 'Company X'
+    }
+]
